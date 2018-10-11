3385b88f35a666a36341a0e38d8cef3e42e77aa6
MaryKattyVO <mkvicunaore@gmail.com>
2018-10-11 16:38:23 -0500

    final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase :{
+    apiKey: "AIzaSyDm33rkRM6H1KrLKb0GjWevySz117sX504",
+    authDomain: "todolist-9cb1f.firebaseapp.com",
+    databaseURL: "https://todolist-9cb1f.firebaseio.com",
+    projectId: "todolist-9cb1f",
+    storageBucket: "",
+    messagingSenderId: "797609475399"
+  }
 };
